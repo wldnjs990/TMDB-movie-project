e39af7da370c7cd591d416f4029d681092fad449
wl990@naver.com <wl990@naver.com>
2025-01-03 18:54:09 +0900

    📝 feat/work-pc : 유튜브API연결(동영상 썸네일 가져오기 용도)

--- src/components/movieInfo/movie_info_modal/MovieInfoModal.tsx
+++ src/components/movieInfo/movie_info_modal/MovieInfoModal.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import { useMovieInfoModalStore } from "../../../store/movieInfoBoxStore";
-import { close_icon, trailer_play_icon } from "../../../assets/images";
+import { close_icon } from "../../../assets/images";
 import { axiosInstance } from "../../../api/axios";
 import MovieInfoModalTop from "./MovieInfoModalTop";
 import { useGenreStore } from "../../../store/genreStore";
+import { Swiper, SwiperSlide } from "swiper/react";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
 export default function MovieInfoModal() {
   const contentRef = useRef<HTMLDivElement>(null);
@@ -19,19 +22,37 @@ export default function MovieInfoModal() {
 
   const genreList = useGenreStore((state) => state.genreList);
 
+  const [videos, setVideos] = useState<T_videos[]>();
+
+  const [thumbnails, setThumbnails] = useState();
+
   const getTrailer = async () => {
     const res = await axiosInstance.get(`/movie/${showingMovieInfo.id}/videos`);
     const data: T_MovieVideos = res.data;
+    setVideos(data.results);
     const trailer: T_videos = data.results.filter(
       (video) => video.type === "Trailer"
     )[0];
     setTrailerKey(trailer.key);
   };
 
+  const getThumnail = async () => {
+    const videoKeys = videos?.map((e) => e.key);
+    const getData = await axios.get(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoKeys}&key=${
+        import.meta.env.VITE_YOUTUBE_KEY
+      }`
+    );
+    const thumbnails = getData.data.items.map((e: any) => {
+      // 임시 any타입 부여
+      return e.snippet.thumbnails.standard.url;
+    });
+    setThumbnails(thumbnails);
+  };
+
   useEffect(() => {
     document.body.style.overflow = "hidden";
-    document.body.style.paddingRight = "21px";
-    console.log(genreList);
+    document.body.style.paddingRight = "19px";
     getTrailer();
 
     return () => {
@@ -39,6 +60,11 @@ export default function MovieInfoModal() {
       document.body.style.paddingRight = "0px";
     };
   }, []);
+  useEffect(() => {
+    if (videos !== undefined) {
+      getThumnail();
+    }
+  }, [videos]);
   return (
     <article className="w-full h-screen fixed top-0 left-0 overflow-y-scroll z-50">
       <article
@@ -51,7 +77,7 @@ export default function MovieInfoModal() {
         }}
       >
         {/* 컨텐츠 */}
-        <article className="relative w-full max-w-[1200px] mx-[30px] mt-[30px] mb-[10px] rounded-lg bg-[#181818]">
+        <article className="relative w-full max-w-[1200px] m-[30px] rounded-lg bg-[#181818]">
           <button
             className="p-[15px] rounded-full bg-black absolute top-[10px] right-[10px] z-10"
             onClick={setMovieInfoModalClose}
@@ -59,7 +85,7 @@ export default function MovieInfoModal() {
             <img src={close_icon} alt="닫기 아이콘" />
           </button>
           <MovieInfoModalTop />
-          <article className="px-[50px] py-[20px]">
+          <article className="px-[50px] py-[20px] flex flex-col gap-[10px]">
             <p>
               <span className="text-[20px] font-bold">장르 : </span>
               {showingMovieInfo.genre_ids.map((id) => (
@@ -71,6 +97,29 @@ export default function MovieInfoModal() {
                 </span>
               ))}
             </p>
+            <p>
+              <span className="text-[20px] font-bold">개봉일 : </span>
+              <span>{showingMovieInfo.release_date}</span>
+            </p>
+            <p>
+              <span className="text-[20px] font-bold">오버뷰 : </span>
+              <span>{showingMovieInfo.overview || "줄거리가 없습니다."}</span>
+            </p>
+            <Swiper spaceBetween={10} slidesPerView={4}>
+              {videos?.map((video, i) => {
+                return (
+                  <SwiperSlide key={uuidv4()}>
+                    {thumbnails && (
+                      <img
+                        src={thumbnails[i]}
+                        alt="유튜브 썸네일"
+                        className="w-[300px]"
+                      />
+                    )}
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
           </article>
         </article>
       </article>
